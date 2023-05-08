6278332b05bcf8c652cd7fc412ac1608b2ab1444
CaioMVargas <caiomvargas@gmail.com>
2023-05-08 16:56:08 -0300

    Finalizando projeto

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import PaginaInicial from "./paginas/PaginaInicial";
 import './styles/estilosGlobais.scss'
-
+import PaginaInicial from "./paginas/PaginaInicial";
 
 export default function App() {
   return <PaginaInicial/>;
---
+++ src/componentes/Rodape/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import instagram from "./instagram.svg";
+import styles from "./Rodape.module.scss";
+
+export default function Rodape() {
+  return (
+    <footer className={styles.rodape}>
+      <div className={styles.rodape__icones}>
+        <a
+          href="https://www.instagram.com/aluraonline/?hl=en"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={instagram} alt="ícone do instagram" />
+        </a>
+      </div>
+      <p>Desenvolvido por Caio Vargas</p>
+    </footer>
+  );
+}
